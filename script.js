24cb3f88766a5172352e8529363eaa5f83d45708
Divoreshia <69974969+Divoreshia@users.noreply.github.com>
2026-06-02 12:48:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
     loop: "true",
     effect: "fade",
     fadeEffect: {crossfade:false},
     speed: 1000,
     delay: 2000,
     autoplay: {enable: true, delay: 7000, disableOnInteraction: false},
-    }
-  );
+    });
+ var swiper2 = new Swiper(".mySwiper2", {
+      loop: true,
+      spaceBetween: 30,
+      thumbs: {swiper: swiper},
+    });
