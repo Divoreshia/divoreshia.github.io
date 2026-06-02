709567b9474c074969effdfebd68135565bd65f7
Divoreshia <69974969+Divoreshia@users.noreply.github.com>
2026-06-02 12:42:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,38 +8,3 @@ var swiper = new Swiper(".mySwiper", {
     autoplay: {enable: true, delay: 7000, disableOnInteraction: false},
     }
   );
-
-
-
-//Intro
-  
-  $(document).ready(function() {
-    // Hiển thị GIF khi trang được tải xong
-    $("#intro").show();
-
-    // Ẩn GIF sau 3 giây (3000 milliseconds)
-    setTimeout(function() {
-        $("#intro").fadeOut("slow");
-    }, 2700);
-});
-
-//Code trộm từ simsimi (và simsimi trộm từ của ai đó) 
-//bring to top
-
-$(document).ready(function(){
-  $("button").click(function(){
-      $("div").animate({left: '250px'});
-  });
-  $("#gototop").hide();
-  $(window).scroll(function(){
-      if($(this).scrollTop()>=100){
-          $("#gototop").show(1000);
-      }
-      else{
-          $("#gototop").hide();
-      }
-      $("#gototop").click(function(){
-          document.documentElement.scrollTop = 0;
-      });
-  })
-});
